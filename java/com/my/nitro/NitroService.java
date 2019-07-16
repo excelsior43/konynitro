69528c983878ae8d9e54671f8cc59df245be0ecf
yasir ahamed syed <sonu.yasir@gmail.com>
2019-07-16 14:59:15 +0300

    Update NitroService.java

--- java/com/my/nitro/NitroService.java
+++ java/com/my/nitro/NitroService.java
@@ -1,8 +1,6 @@
 package com.my.nitro;
 
-import org.springframework.beans.BeansException;    
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+
 import com.konylabs.middleware.controller.DataControllerRequest;
 import com.konylabs.middleware.controller.DataControllerResponse;
 import com.konylabs.middleware.dataobject.Result;
@@ -12,22 +10,14 @@ import com.konylabs.middleware.dataobject.Result;
  *
  */
 
-public class NitroService implements ServiceInterface, ApplicationContextAware { 
+public class NitroService implements ServiceInterface { 
 	
 	public Result invoke(String methodId, Object[] inputObjects)  throws java.lang.Exception{
 		NitroObjectFactory factory=new NitroObjectFactory();
 		return factory.invoke(this, methodId, inputObjects); 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
-	 */
 	
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext=applicationContext;
-		
-	}
-	private ApplicationContext applicationContext=null;
 	
 	/* (non-Javadoc)
 	 * @see com.konylabs.middleware.common.JavaService2#invoke(java.lang.String, java.lang.Object[], com.konylabs.middleware.controller.DataControllerRequest, com.konylabs.middleware.controller.DataControllerResponse)
